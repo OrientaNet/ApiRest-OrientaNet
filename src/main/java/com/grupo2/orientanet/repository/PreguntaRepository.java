7f219f5109e016807eec2cc1cf7f9c198e5475dd
luisgaribay1 <ltandaypang1@upao.edu.pe>
2024-09-16 21:41:42 -0500

    fead:crud-pregunta

--- src/main/java/com/grupo2/orientanet/repository/PreguntaRepository.java
+++ src/main/java/com/grupo2/orientanet/repository/PreguntaRepository.java
@@ -1,4 +1,9 @@
 package com.grupo2.orientanet.repository;
 
-public interface PreguntaRepository {
+import com.grupo2.orientanet.model.entity.Pregunta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PreguntaRepository extends JpaRepository<Pregunta, Long> {
 }
--- src/main/java/com/grupo2/orientanet/service/PreguntaService.java
+++ src/main/java/com/grupo2/orientanet/service/PreguntaService.java
@@ -1,4 +1,16 @@
 package com.grupo2.orientanet.service;
 
+import com.grupo2.orientanet.model.entity.Pregunta;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PreguntaService {
+    List<Pregunta> listarPreguntas();
+
+    Optional<Pregunta> obtenerPreguntaPorId(Long id);
+
+    Pregunta guardarPregunta(Pregunta pregunta);
+
+    void eliminarPregunta(Long id);
 }
