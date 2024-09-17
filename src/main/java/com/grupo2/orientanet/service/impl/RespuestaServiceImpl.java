8d9bf18ea4e0b84922db4a897d633d51a1680073
luisgaribay1 <ltandaypang1@upao.edu.pe>
2024-09-16 19:17:43 -0500

    feat: crud de la tabla Respuesta

--- src/main/java/com/grupo2/orientanet/service/impl/RespuestaServiceImpl.java
+++ src/main/java/com/grupo2/orientanet/service/impl/RespuestaServiceImpl.java
@@ -1,4 +1,37 @@
 package com.grupo2.orientanet.service.impl;
 
-public class RespuestaServiceImpl {
+import com.grupo2.orientanet.model.entity.Respuesta;
+import com.grupo2.orientanet.repository.RespuestaRepository;
+import com.grupo2.orientanet.service.RespuestaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RespuestaServiceImpl implements RespuestaService {
+
+    @Autowired
+    private RespuestaRepository respuestaRepository;
+
+    @Override
+    public List<Respuesta> listarRespuestas() {
+        return respuestaRepository.findAll();
+    }
+
+    @Override
+    public Optional<Respuesta> obtenerRespuestaPorId(Long id) {
+        return respuestaRepository.findById(id);
+    }
+
+    @Override
+    public Respuesta guardarRespuesta(Respuesta respuesta) {
+        return respuestaRepository.save(respuesta);
+    }
+
+    @Override
+    public void eliminarRespuesta(Long id) {
+        respuestaRepository.deleteById(id);
+    }
 }
