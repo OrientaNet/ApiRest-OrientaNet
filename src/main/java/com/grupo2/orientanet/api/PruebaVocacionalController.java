ff79fef8eb5d0d98c622d749eefd6ec753308f71
villitaqt <diegovillaju11@gmail.com>
2024-11-29 02:28:27 -0500

    parche: endpoint de pruebas to public

--- src/main/java/com/grupo2/orientanet/api/PruebaVocacionalController.java
+++ src/main/java/com/grupo2/orientanet/api/PruebaVocacionalController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -59,6 +60,7 @@ public class PruebaVocacionalController {
 
 
     // Endpoint para realizar la prueba vocacional
+    @PreAuthorize("hasRole('ESTUDIANTE')")
     @PostMapping("/{estudianteId}/realizar/{pruebaId}")
     public ResponseEntity<ResultadoTest> realizarPrueba(
             @PathVariable Long pruebaId,
